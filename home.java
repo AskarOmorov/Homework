0767d76ae69118f4c4224bb8d01ba98dc1c439f5
AskarOmorov <136386332+AskarOmorov@users.noreply.github.com>
2023-06-16 15:11:06 +0200

    Add files via upload

---
+++ home.java
@@ -0,0 +1,44 @@
+public class home {
+    public static void main(String[] args) {
+        System.out.println("Работу выполнил Оморов Аскар");
+
+        boolean b = true;
+        byte bb = 121;
+        short s = 342;
+        int i = 18654;
+        long l = 765577L;
+        float f = 15.78676F;
+        double d = 9496974627.79723696;
+        char c = '1';
+        System.out.println(b);
+        System.out.println(bb);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        b = false;
+        bb = 12;
+        s = 346;
+        i = 18676;
+        l = 7655897L;
+        f = 15.78654F;
+        d = 94969753227.797238766;
+        c = 'j';
+        System.out.println(b);
+        System.out.println(bb);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
